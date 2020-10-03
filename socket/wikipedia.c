b9ba9f396ba7840dc1e0e54db2e9c5106131d27f
Gabriel110 <gabrielcorreiadev@gmail.com>
2020-10-03 15:25:35 -0300

    Cliente wikipedia

--- socket/wikipedia.c
+++ socket/wikipedia.c
@@ -22,7 +22,7 @@ int open_socket(char *host,char *port){
   struct addrinfo *res;
   struct addrinfo hints;
   memset(&hints,0,sizeof(hints));
-  hints.ai_family = PF_UNSPEC;
+  hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   if(getaddrinfo(host,port,&hints,&res) == -1){
     errors("Can`t resolve the address");
@@ -49,7 +49,7 @@ int main(int argc, char *argv[]){
   int d_sock;
   d_sock = open_socket("en.wikipedia.org","80");
   char buff[255];
-  sprintf(buff,"GET /wiki/%s http/1.1\r\n",argv[1]);
+  sprintf(buff,"GET /wiki/%s http/3.0\r\n",argv[1]);
   say(d_sock,buff);
 
   say(d_sock,"Host: en.wikipedia.org\r\n\r\n");
